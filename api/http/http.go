41159f50bf2afaf96a7750fbad034312e1ba5222
Ilia Frenkel <frenkel.ilia@gmail.com>
2021-06-17 18:11:41 +1000

    Proper json request parsing.
    From here:
    https://www.alexedwards.net/blog/how-to-properly-parse-a-json-request-body

--- api/http/http.go
+++ api/http/http.go
@@ -4,8 +4,12 @@ import (
 	"crypto/md5"
 	"crypto/rand"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -48,19 +52,101 @@ func (h *ApiHandler) handlePaste(w http.ResponseWriter, r *http.Request) {
 
 func (h *ApiHandler) handleCreate(w http.ResponseWriter, r *http.Request) {
 	// Parse incoming json
-	// TODO: needs improvement as per
 	// https://www.alexedwards.net/blog/how-to-properly-parse-a-json-request-body
-	var data struct {
-		Title string
-		Body  []byte
-	}
-	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-		fmt.Println(fmt.Errorf("failed to parse the form: %v", err))
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+
+	// If the Content-Type header is present, check that it has the value
+	// application/json.
+	if h := r.Header.Get("Content-Type"); h != "" {
+		if h != "application/json" {
+			msg := "Content-Type header is not application/json"
+			http.Error(w, msg, http.StatusUnsupportedMediaType)
+			return
+		}
 	}
 
-	fmt.Println(data)
+	// Use http.MaxBytesReader to enforce a maximum read of 10KB from the
+	// response body. A request body larger than that will now result in
+	// Decode() returning a "http: request body too large" error.
+	r.Body = http.MaxBytesReader(w, r.Body, 10240)
+
+	// Setup the decoder and call the DisallowUnknownFields() method on it.
+	// This will cause Decode() to return a "json: unknown field ..." error
+	// if it encounters any extra unexpected fields in the JSON. Strictly
+	// speaking, it returns an error for "keys which do not match any
+	// non-ignored, exported fields in the destination".
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	var data api.Paste
+	if err := dec.Decode(&data); err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+
+		switch {
+		// Catch any syntax errors in the JSON and send an error message
+		// which interpolates the location of the problem to make it
+		// easier for the client to fix.
+		case errors.As(err, &syntaxError):
+			msg := fmt.Sprintf("Request body contains badly-formed JSON (at position %d)", syntaxError.Offset)
+			http.Error(w, msg, http.StatusBadRequest)
+
+		// In some circumstances Decode() may also return an
+		// io.ErrUnexpectedEOF error for syntax errors in the JSON. There
+		// is an open issue regarding this at
+		// https://github.com/golang/go/issues/25956.
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			msg := "Request body contains badly-formed JSON"
+			http.Error(w, msg, http.StatusBadRequest)
+
+		// Catch any type errors, like trying to assign a string in the
+		// JSON request body to a int field in our Person struct. We can
+		// interpolate the relevant field name and position into the error
+		// message to make it easier for the client to fix.
+		case errors.As(err, &unmarshalTypeError):
+			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
+			http.Error(w, msg, http.StatusBadRequest)
+
+		// Catch the error caused by extra unexpected fields in the request
+		// body. We extract the field name from the error message and
+		// interpolate it in our custom error message. There is an open
+		// issue at https://github.com/golang/go/issues/29035 regarding
+		// turning this into a sentinel error.
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
+			http.Error(w, msg, http.StatusBadRequest)
+
+		// An io.EOF error is returned by Decode() if the request body is
+		// empty.
+		case errors.Is(err, io.EOF):
+			msg := "Request body must not be empty"
+			http.Error(w, msg, http.StatusBadRequest)
+
+		// Catch the error caused by the request body being too large. Again
+		// there is an open issue regarding turning this into a sentinel
+		// error at https://github.com/golang/go/issues/30715.
+		case err.Error() == "http: request body too large":
+			msg := "Request body must not be larger than 10KB"
+			http.Error(w, msg, http.StatusRequestEntityTooLarge)
+
+		// Otherwise default to logging the error and sending a 500 Internal
+		// Server Error response.
+		default:
+			log.Println(err.Error())
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		return
+	}
+	// Call decode again, using a pointer to an empty anonymous struct as
+	// the destination. If the request body only contained a single JSON
+	// object this will return an io.EOF error. So if we get anything else,
+	// we know that there is additional data in the request body.
+
+	if err := dec.Decode(&struct{}{}); err != io.EOF {
+		msg := "Request body must only contain a single JSON object"
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
 
 	// Create new paste
 	b := make([]byte, 16)
@@ -83,7 +169,6 @@ func (h *ApiHandler) handleCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "%s", res)
-
 }
 
 func (h *ApiHandler) handleDelete(w http.ResponseWriter, r *http.Request) {
