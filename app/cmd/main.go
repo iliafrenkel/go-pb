ba99d1522c2c7430f4cf3511ea9e9afb38adcf1c
Ilia Frenkel <frenkel.ilia@gmail.com>
2021-07-02 05:15:52 +0000

    Started to work on the frontend.

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,17 +1,72 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"path/filepath"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := mux.NewRouter()
-	staticFilesDirectory := http.Dir("../assets/")
-	staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFilesDirectory))
-	r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+	// // Load templates
+	// pattern := filepath.Join("..", "templates", "*.html")
+	// templates := template.Must(template.ParseGlob(pattern))
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	// // Define static assets location
+	// r := mux.NewRouter()
+	// staticFilesDirectory := http.Dir("../assets/")
+	// staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFilesDirectory))
+	// r.PathPrefix("/assets/").Handler(staticFileHandler).Methods("GET")
+
+	// // Define routes
+	// r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	if err := templates.ExecuteTemplate(w, "index", nil); err != nil {
+	// 		log.Fatalf("Failed to execute index template: %s", err)
+	// 	}
+
+	// }).Methods("GET")
+
+	// log.Fatal(http.ListenAndServe(":8080", r))
+
+	router := gin.Default()
+
+	router.LoadHTMLGlob(filepath.Join("..", "templates", "*.html"))
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"index.html",
+			gin.H{
+				"title": " Go PB - Home",
+			},
+		)
+	})
+
+	router.GET("/u/login", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"login.html",
+			gin.H{
+				"title": " Go PB - Login",
+			},
+		)
+	})
+
+	router.GET("/u/register", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"register.html",
+			gin.H{
+				"title": " Go PB - Register",
+			},
+		)
+	})
+
+	router.Run("127.0.0.1:8080")
 }
